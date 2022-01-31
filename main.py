e1da4174c9d167ee9e6b3e962a137bff43c5e378
U5ER-N4ME <1477883894@qq.com>
2022-01-31 15:30:29 +0800

    I/O optimizing
    Asks when target dir exists
    Name output files with both index and time segment

--- main.py
+++ main.py
@@ -20,14 +20,32 @@ if __name__ == "__main__":
     fragments = xp.getFragment(timeStat)
 
     # create a new directory
-    os.mkdir(fileName[0])
+    try: 
+        os.mkdir(fileName[0])
+    except:
+        log(3, 'Target directory already exists. Type y to continue, else exit')
+        choice = input()
+        if choice == 'Y' or choice == 'y':
+            pass
+        else:
+            sys.exit(0)
+    else:
+        pass
 
     # clip
     index = 0
+    indexLength = len(str(len(fragments)))
     for frag in fragments:
         index = index + 1
+        startTime = str((frag[0] - 9) // 3600).zfill(1) + '.' + \
+                    str(((frag[0] - 9) % 3600) // 60).zfill(2) + '.' + \
+                    str((frag[0] - 9) % 60).zfill(2)
+        endTime   = str(frag[1] // 3600).zfill(1) + '.' + \
+                    str((frag[1] % 3600) // 60).zfill(2) + '.' + \
+                    str(frag[1] % 60).zfill(2)
+
         vp.clip(fileName[0] + '.' + fileName[1], 
                 frag[0] - 9, frag[1], 
-                fileName[0] + '\\' + str(index) + '.mp4'
+                fileName[0] + '\\' + str(index).zfill(indexLength) + '_' + startTime + '-' + endTime + '.flv'
                 )
     log(2, index, ' video(s) clipped')
