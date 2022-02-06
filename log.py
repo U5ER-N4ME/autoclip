0a8797bdcd046322b77339d4d69525341001bed6
U5ER-N4ME <1477883894@qq.com>
2022-02-06 12:04:17 +0800

    Allow log to file
    Allow output log to log.txt

--- log.py
+++ log.py
@@ -1,12 +1,23 @@
-logLevel = 2
+terminalLogLevel = 2
+fileLogLevel = 5
 logLevelList = ['TMP', 'DBG', 'INF', 'WRN', 'ERR']
 
 def log(level, *content):
-    if level >= logLevel:
+    # log output to terminal
+    if level >= terminalLogLevel:
         print ('[' + logLevelList[level] + '] ', end = '')
         for item in content:
             print(item, end = '')
         print ()
     else:
         pass
+    
+    # log to file
+    if level >= fileLogLevel:
+        with open('log.txt', 'a', encoding='utf-8', errors='ignore') as logfile:
+            logfile.write('[' + logLevelList[level] + '] ')
+            for item in content:
+                logfile.write(str(item))
+            logfile.write('\n')
+
 
